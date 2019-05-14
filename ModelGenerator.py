c583832890df71d23b41c5c83669398db2cd658a
markalos <goknowy@gmail.com>
2019-05-14 21:42:54 +0800

    of code

--- ModelGenerator.py
+++ ModelGenerator.py
@@ -20,6 +20,7 @@ class ModelGenerator(BaseGenerator):
 	def getModels(self):
 		
 		entityFields = self.entity['fields']
+		self.entityName = self.entity['name']
 
 		modelFields = []
 		for key in entityFields.keys():
@@ -33,9 +34,6 @@ class ModelGenerator(BaseGenerator):
 		for key in entityFields.keys():
 			names = entityFields[key]['refed']
 			modelNames.update(names)
-
-		
-		print(allFields, 'allfileds')
 
 		for name in modelNames:
 			modelFields = []
@@ -54,6 +52,8 @@ class ModelGenerator(BaseGenerator):
 		self.classStartLine = "public class {} {{".format(utils.upperFirst(model["name"]))
 		lines = [self.packageLine, utils.empty_line,self.classStartLine]
 		lines.extend(self.generateFiledLies(model['fields']))
+		if model['name'] != self.entityName:
+			lines.extend(self.mapOf(model))
 		lines.append(self.endLine)
 		return '\n'.join(lines)
 
@@ -63,5 +63,19 @@ class ModelGenerator(BaseGenerator):
 			lines.append(self.lineForField(field))
 		lines.append(utils.empty_line)
 		return lines
+
+	def mapOf(self, model):
+		
+		ofStartLine = '\tpublic static {} of({} {}) {{'
+		ofStartCode = ofStartLine.format(utils.upperFirst(model['name']), utils.upperFirst(self.entityName), self.entityName)
+		declareLine = '\t\tvar {} = new {}'
+		declareCode = declareLine.format(model['name'], utils.upperFirst(model['name']))
+		codes = [ofStartCode]
+		ofBodyLine = '\t\t{}.{}({}.{});'
+		bodyCodes = [ofBodyLine.format(model['name'], self.generateSetter(field), self.entityName, self.generateGetter(field)) for field in model['fields']]
+		codes.extend(bodyCodes)
+		codes.append('\t}')
+		return codes
+
 
 		
